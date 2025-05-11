ef11aa07fded02af3ab23dc2fc0896400b4b6d1d
mithlesh prasad <101449591+mithleshprasad@users.noreply.github.com>
2025-05-11 22:19:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,12 +36,9 @@ app.use(cors({
 //   cookie: { secure: false } 
 // }));
 // Database connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected successfully'))
-.catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
 const authRoutes = require('./routes/auth');
